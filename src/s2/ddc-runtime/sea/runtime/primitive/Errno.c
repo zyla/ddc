fbd39ead5f3aad7276ef0d5f92e5554ce37d8b93
Maciej Bielecki <zyla@prati.pl>
2018-03-31 20:10:03 +0200

    Fix ddcPrimErrnoShowMessage crash on glibc
    GNU strerror_r behaves differently than the standard one, which resulted
    in an abort when using errno_showMessage on Linux. This patch fixes
    that.

--- src/s2/ddc-runtime/sea/runtime/primitive/Errno.c
+++ src/s2/ddc-runtime/sea/runtime/primitive/Errno.c
@@ -16,21 +16,29 @@ int     ddcPrimErrnoGet ()
         return errno;
 }
 
-
 // Get the name of the given 'errno' value.
 Obj*    ddcPrimErrnoShowMessage (int errno_val)
 {
         // Write message into a temporary buffer.
         char* pBuf      = alloca(1024);
+
+#ifdef __GLIBC__
+        // GNU strerror_r returns a pointer to the result.
+        char *pErrorMsg = strerror_r(errno_val, pBuf, 1024);
+        if(!pErrorMsg) abort();
+#else
+        // XSI version always copies to the provided buffer and returns 0 on success.
         if (strerror_r(errno_val, pBuf, 1024) != 0) abort();
+        char *pErrorMsg = pBuf;
+#endif
 
         // Allocate a new vector to hold the message.
-        int lenActual   = strlen(pBuf);
+        int lenActual   = strlen(pErrorMsg);
         Obj* pVec       = ddcPrimVectorAlloc8(lenActual + 1);
         string_t* pPay  = (string_t*)ddcPrimVectorPayload8(pVec);
 
         // Copy data into the new vector.
-        strncpy(pPay, pBuf, lenActual);
+        strncpy(pPay, pErrorMsg, lenActual);
 
         // Ensure there's a null character on the end of the string.
         *(pPay + lenActual) = 0;
